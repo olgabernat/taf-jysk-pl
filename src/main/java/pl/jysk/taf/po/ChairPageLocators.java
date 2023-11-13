27545ee791323b33db1f7ca4237dd28ef80c9d68
Olga Bernat <olga.alexenok@gmail.com>
2023-11-13 17:52:15 +0100

    refactor search test

--- src/main/java/pl/jysk/taf/po/ChairPageLocators.java
+++ src/main/java/pl/jysk/taf/po/ChairPageLocators.java
@@ -2,7 +2,7 @@ package pl.jysk.taf.po;
 
 public class ChairPageLocators {
     public static final String CENA_FILTER_LOCATOR = "//span[text()='Cena']";
-    public static final String MIN_PRICE_FILTER_LOCATOR = "//input[@placeholder='Min']";
+    public static final String MIN_PRICE_LOCATOR = "//input[@placeholder='Min']";
     public static final String MAX_PRICE_LOCATOR = "//input[@placeholder='Max']";
     public static final String CATEGORY_LOCATOR = "//span[text()='Kategoria']";
     public static final String CHAIR_TEXT_LOCATOR = "//a[@class = 'product-teaser-title text-reset']";
