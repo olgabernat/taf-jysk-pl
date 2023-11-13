24a81254b12f4208217444e04cc1105b4de44b5c
Olga Bernat <olga.alexenok@gmail.com>
2023-11-13 16:50:59 +0100

    added ui tests on Login page

--- src/main/java/pl/jysk/taf/po/LoginPageLocators.java
+++ src/main/java/pl/jysk/taf/po/LoginPageLocators.java
@@ -1,5 +1,9 @@
 package pl.jysk.taf.po;
 
 public class LoginPageLocators {
-    public static final String HEADER_TEXT_LODCATOR = "//p[@class='text-header']";
+    public static final String HEADER_TEXT_LOCATOR = "//p[@class='text-header']";
+    public static final String EMAIL_INPUT_LOCATOR = "//input[@name='email']";
+    public static final String PASSWORD_INPUT_LOCATOR = "//input[@name='password']";
+    public static final String LOGIN_BUTTON_LOCATOR = "//button[@type='submit' and span[text()='Zaloguj się']]";
+    public static final String ERROR_TEXT_LOCATOR = "//div[@class='form-errors']";
 }
--- src/test/java/pl/jysk/taf/ui/LoginPageTest.java
+++ src/test/java/pl/jysk/taf/ui/LoginPageTest.java
@@ -5,14 +5,28 @@ import org.junit.jupiter.api.Test;
 import pl.jysk.taf.po.HomePage;
 import pl.jysk.taf.po.LoginPage;
 
-public class LoginPageTest extends BaseTest{
+public class LoginPageTest extends BaseTest {
     @Test
     public void testLoginPageOpened() {
         HomePage homePage = new HomePage();
         homePage.clickButtonLogin();
         LoginPage loginPage = new LoginPage();
         String actualCopywriterText = loginPage.getCopyright();
-        String extendsCopywriterText = "Zaloguj się";
-        Assertions.assertEquals(extendsCopywriterText, actualCopywriterText);
+        String expectedCopywriterText = "Zaloguj się";
+        Assertions.assertEquals(expectedCopywriterText, actualCopywriterText);
+    }
+
+    @Test
+    public void testLoginNotExistUser() {
+        HomePage homePage = new HomePage();
+        homePage.clickButtonLogin();
+        LoginPage loginPage = new LoginPage();
+        loginPage.enterEmail("test@test.com")
+                .enterPassword("test123")
+                .clickLoginButton();
+
+        String actualError = loginPage.getErrorMessage();
+        String expectedError = "Login of test@test.com failed";
+        Assertions.assertEquals(expectedError, actualError);
     }
 }
