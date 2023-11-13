dba32e8a6d5122c12584a37af37d3af64becc428
olgabernat <141057027+olgabernat@users.noreply.github.com>
2023-11-13 16:51:40 +0100

    added ui tests on Login page (#2)

--- src/main/java/pl/jysk/taf/po/LoginPage.java
+++ src/main/java/pl/jysk/taf/po/LoginPage.java
@@ -4,13 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pl.jysk.taf.singleton.Singleton;
 
 import java.time.Duration;
 
-import static pl.jysk.taf.po.LoginPageLocators.HEADER_TEXT_LODCATOR;
+import static pl.jysk.taf.po.HomePageLocators.INPUT_SEARCH_LINE_LOCATOR;
+import static pl.jysk.taf.po.LoginPageLocators.*;
 
 public class LoginPage {
     private WebDriver driver;
@@ -23,7 +25,31 @@ public class LoginPage {
     public String getCopyright() {
         logger.info("Copywrite text on the Login page received");
         return new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(HEADER_TEXT_LODCATOR)))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(HEADER_TEXT_LOCATOR)))
+                .getText();
+    }
+
+    public LoginPage enterEmail (String email) {
+        WebElement emailInputElement = driver.findElement(By.xpath(EMAIL_INPUT_LOCATOR));
+        emailInputElement.sendKeys(email);
+        return this;
+    }
+
+    public LoginPage enterPassword (String password) {
+        WebElement passwordInputElement = driver.findElement(By.xpath(PASSWORD_INPUT_LOCATOR));
+        passwordInputElement.sendKeys(password);
+        return this;
+    }
+
+    public LoginPage clickLoginButton () {
+        WebElement loginButtonElement = driver.findElement(By.xpath(LOGIN_BUTTON_LOCATOR));
+        loginButtonElement.click();
+        return this;
+    }
+
+    public String getErrorMessage () {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(ERROR_TEXT_LOCATOR)))
                 .getText();
     }
 }
